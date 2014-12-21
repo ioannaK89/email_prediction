60dbeaa518ec993ccef529fd385fd0439a97f862
Joanna <ioannak89@gmail.com>
2014-12-21 22:00:45 +0200

    add email prediction class

--- lib/email_predictor.rb
+++ lib/email_predictor.rb
@@ -1,2 +1,71 @@
+require_relative 'models/user'
+
 class EmailPredictor
+
+  attr_reader :username, :domain
+
+  def initialize(username, domain)
+    @username = username
+    @domain = domain
+  end
+
+  def predict
+    validate!
+
+    User.domain_exists?(@domain) ? find_prediction_for_domain : all_predictions
+  end
+
+  private
+
+  def find_prediction_for_domain
+    user_from_db = User.find_by_domain(@domain)
+
+    prediction = case user_from_db.email
+                 when /^\w\.\w@#{@domain}$/i
+                   first_initial_dot_last_initial
+                 when /^\w{2,}\.\w@#{@domain}$/i
+                   first_name_dot_last_initial
+                 when /^\w\.\w{2,}@#{@domain}$/i
+                   first_initial_dot_last_name
+                 when /^\w{2,}\.\w{2,}@#{@domain}$/i
+                   first_name_dot_last_name
+                 end
+
+    "#{prediction}@#{@domain}"
+  end
+
+  def all_predictions
+    [
+      first_name_dot_last_name,
+      first_name_dot_last_initial,
+      first_initial_dot_last_name,
+      first_initial_dot_last_initial
+    ].map{|name| "#{name}@#{@domain}"}
+  end
+
+  def user
+    @user ||= User.new(@username)
+  end
+
+  def first_name_dot_last_name
+    "#{user.first_name}.#{user.last_name}"
+  end
+
+  def first_name_dot_last_initial
+    "#{user.first_name}.#{user.last_name[0]}"
+  end
+
+  def first_initial_dot_last_name
+    "#{user.first_name[0]}.#{user.last_name}"
+  end
+
+  def first_initial_dot_last_initial
+    "#{user.first_name[0]}.#{user.last_name[0]}"
+  end
+
+  def validate!
+    raise 'Missing username' if @username.nil?
+    raise 'Missing domain' if @domain.nil?
+  end
+
 end
--- spec/lib/email_predictor_spec.rb
+++ spec/lib/email_predictor_spec.rb
@@ -1,5 +1,108 @@
 require 'spec_helper'
 
 describe EmailPredictor do
-  
+  let(:predictor) { described_class.new(username, domain) }
+  let(:username)  { 'John Ferguson' }
+  let(:domain)    { 'alphasights.com' }
+
+  describe '.new' do
+    it 'assigns a user name' do
+      expect(predictor.username).to eq(username)
+    end
+
+    it 'assigns a domain' do
+      expect(predictor.domain).to eq(domain)
+    end
+  end
+
+  describe '#predict' do
+    context 'When username is missing' do
+      let(:username) { nil }
+
+      it 'raises an exception' do
+        expect { predictor.predict }.to raise_error(RuntimeError, 'Missing username')
+      end
+    end
+
+    context 'When domain is missing' do
+      let(:domain) { nil }
+
+      it 'raises an exception' do
+        expect { predictor.predict }.to raise_error(RuntimeError, 'Missing domain')
+      end
+    end
+
+    context 'When domain doesnt exists in the database' do
+      before do
+        allow(User).to receive(:all).and_return([])
+      end
+
+      let(:expected_predictions) do
+        [
+          "john.ferguson@alphasights.com",
+          "john.f@alphasights.com",
+          "j.ferguson@alphasights.com",
+          "j.f@alphasights.com"
+        ]
+      end
+
+      it "should return 4 possible predictions" do
+        expect(predictor.predict).to eq(expected_predictions)
+      end
+    end
+
+    context 'When domain exists in the database' do
+      context 'When the format of email is: first_name_dot_last_name' do
+
+        before do
+          allow(User).to receive(:all).and_return([user])
+        end
+
+        let(:user) { User.new('Joanna Kostaki', 'joanna.kostaki@alphasights.com') }
+
+        it 'should return first_name_dot_last_name prediction' do
+          expect(predictor.predict).to eq('john.ferguson@alphasights.com')
+        end
+      end
+
+      context 'When the format of email is: first_name_dot_last_initial' do
+
+        before do
+          allow(User).to receive(:all).and_return([user])
+        end
+
+        let(:user) { User.new('Joanna Kostaki', 'joanna.k@alphasights.com') }
+
+        it 'should return first_name_dot_last_initial prediction' do
+          expect(predictor.predict).to eq('john.f@alphasights.com')
+        end
+      end
+
+      context 'When the format of email is: first_initial_dot_last_initial' do
+
+        before do
+          allow(User).to receive(:all).and_return([user])
+        end
+
+        let(:user) { User.new('Joanna Kostaki', 'j.k@alphasights.com') }
+
+        it 'should return first_initial_dot_last_initial prediction' do
+          expect(predictor.predict).to eq('j.f@alphasights.com')
+        end
+      end
+
+      context 'When the format of email is: first_initial_dot_last_name' do
+
+        before do
+          allow(User).to receive(:all).and_return([user])
+        end
+
+        let(:user) { User.new('Joanna Kostaki', 'j.kostaki@alphasights.com') }
+
+        it 'should return first_initial_dot_last_name prediction' do
+          expect(predictor.predict).to eq('j.ferguson@alphasights.com')
+        end
+      end
+    end
+  end
 end
